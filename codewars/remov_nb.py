ed857e6c517cccfa4fc6562bb925f9c8e5c2bdf2
unknown <alfir.kalimullin@yandex.ru>
2022-02-02 02:31:04 +0500

    02/02/2022

--- codewars/remov_nb.py
+++ codewars/remov_nb.py
@@ -1,19 +1,21 @@
 # https://www.codewars.com/kata/5547cc7dcad755e480000004
+from audioop import reverse
+import time
+
+
 def remov_nb(n):
-    arr = []
     needed = (n)/2*(n+1)
-    flag = False
-    for a in range(n+1):
-        if flag:
-            break
-        for b in range(a):
-            print(a,b)
+    # print(needed)
+    for a in reversed(range(n)):
+        for b in reversed(range(a, n+1)):
+            # print(a+1, b)
             test = a*b + a + b
             if needed == test:
-                arr.append((b,a))
-                arr.append((a,b))
-                flag = True
-                break
-    return arr
+                return [(a, b), (b, a)]
+    return []
+# 1000003
 
-print(remov_nb(26))
+
+start = time.perf_counter()
+print(remov_nb(101))
+print(time.perf_counter() - start)
